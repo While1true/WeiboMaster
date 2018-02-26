b8c46012749b3fa0de79486933e062d45c3930d1
公司 <951827860@qq.com>
2018-02-26 11:13:34 +0800

    weibo

---
+++ app/src/main/java/com/master/weibomaster/Fragment/CategoryF.kt
@@ -0,0 +1,15 @@
+package com.master.weibomaster.Fragment
+
+import android.os.Bundle
+import com.master.weibomaster.R
+import coms.pacs.pacs.BaseComponent.BaseFragment
+
+/**
+ * Created by 不听话的好孩子 on 2018/2/26.
+ */
+class CategoryF : BaseFragment() {
+    override fun getLayoutId()=R.layout.recyclerview
+
+    override fun init(savedInstanceState: Bundle?) {
+    }
+}
