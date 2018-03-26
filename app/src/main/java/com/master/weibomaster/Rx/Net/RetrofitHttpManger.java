4a35824d89fecc3c2f31f24b13857fba163f8287
公司 <951827860@qq.com>
2018-03-26 08:38:00 +0800

    增加搜索

--- app/src/main/java/com/master/weibomaster/Rx/Net/RetrofitHttpManger.java
+++ app/src/main/java/com/master/weibomaster/Rx/Net/RetrofitHttpManger.java
@@ -23,8 +23,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class RetrofitHttpManger {
-    private static final int DEFAULT_CONNECT_TIMEOUT = 12;
-    private static final int DEFAULT_READ_TIMEOUT = 12;
+    private static final int DEFAULT_CONNECT_TIMEOUT = 6;
+    private static final int DEFAULT_READ_TIMEOUT = 8;
     private static final String BASEURL = "http://10.0.110.134:8090/masterWeiBo/";
     private Retrofit mRetrofit;
 
